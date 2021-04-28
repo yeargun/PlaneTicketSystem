0bba967fd44213caa79cce1dd2490c7cb1dd6e86
Ali Argun Sayılgan <44312765+yeargun@users.noreply.github.com>
2021-04-28 13:01:09 +0300

    Add files via upload

---
+++ Ass3/as3/src/Airport.java
@@ -0,0 +1,9 @@
+public class Airport {
+    private String cityName;
+    private String[] aliases; 
+    
+    Airport(String cityName, String[] aliases){
+        this.cityName=cityName;
+        this.aliases=aliases;
+    }
+}
