4c01998a5d5b01c3454b083588b5a1aedbc3e15b
Ali Argun Sayılgan <44312765+yeargun@users.noreply.github.com>
2021-11-06 19:56:58 +0300

    Delete Ass3/as3 directory

--- Ass3/as3/src/Airport.java
+++
@@ -1,9 +0,0 @@
-public class Airport {
-    private String cityName;
-    private String[] aliases; 
-    
-    Airport(String cityName, String[] aliases){
-        this.cityName=cityName;
-        this.aliases=aliases;
-    }
-}
